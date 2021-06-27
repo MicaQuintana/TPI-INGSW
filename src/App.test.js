916fe5bc7f8656a8bf8f99050010b72a33af4936
Micaela <micaelaquintana90@gmail.com>
2021-06-27 19:03:29 -0300

    otra prueba

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Project by Micaela Quintana - Ing. Sw. UTN FRRe/i);
+  const linkElement = screen.getByText(/Project by Micaela Quintana - Ing. SW. UTN FRRe/i);
   expect(linkElement).toBeInTheDocument();
 });
